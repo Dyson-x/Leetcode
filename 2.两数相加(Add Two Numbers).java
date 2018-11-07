ee74869dfe1d0d352a6dc313d19cf34b0f63ce1d
Dyson <15229907309@sina.cn>
2018-11-07 18:05:52 +0800

    2：两数相加

--- 2.两数相加(Add Two Numbers).java
+++ 2.两数相加(Add Two Numbers).java
@@ -20,6 +20,7 @@ class Solution {
             //计算进位
             cur=num/10;
             ret.val=digit;
+            //更新l1和l2链表
             l1=l1!=null?l1.next:l1;
             l2=l2!=null?l2.next:l2;
             //判断是否需要添加新节点
